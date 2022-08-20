67f06e387b0765f0d92d54606f0908f9438df0e6
Emmszhii <emmanuelmanalo707@gmail.com>
2022-08-20 23:02:17 +0800

    Level 5: Using passport.js to add cookies and session

--- app.js
+++ app.js
@@ -2,25 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, 'Please insert a username!'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Please insert a password!'],
-  },
-});
-
-const User = new mongoose.model('User', userSchema);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -29,8 +15,57 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.use(
+  session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {},
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
+
+// mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    // required: [true, 'Please insert a username!'],
+  },
+  password: {
+    type: String,
+    // required: [true, 'Please insert a password!'],
+  },
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get('/', (req, res) => {
-  res.render('home');
+  if (req.isAuthenticated()) {
+    res.redirect('/secrets');
+  } else {
+    res.render('home');
+  }
+});
+
+app.get('/secrets', (req, res) => {
+  // session user algorithm
+  if (req.isAuthenticated()) {
+    res.render('secrets');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app
@@ -38,22 +73,26 @@ app
   .get((req, res) => {
     res.render('login');
   })
-  .post((req, res) => {
-    User.findOne(
-      {
-        email: req.body.username,
-      },
-      (err, result) => {
-        err && console.log(err);
+  .post(
+    passport.authenticate('local', {
+      successRedirect: '/secrets',
+      failureRedirect: '/login',
+    }),
+    (req, res) => {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+      });
 
-        if (result) {
-          result.password === md5(req.body.password)
-            ? res.render('secrets')
-            : res.render('login');
-        }
-      }
-    );
-  });
+      // login algorithm
+      req.login(user, (err) => {
+        err && console.log(err);
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets');
+        });
+      });
+    }
+  );
 
 app
   .route('/register')
@@ -61,18 +100,29 @@ app
     res.render('register');
   })
   .post((req, res) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password),
-    });
-
-    newUser.save((err) => {
-      !err ? res.render('secrets') : console.log(err);
-    });
+    //register algorithm
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          console.log(err);
+          res.redirect('/register');
+        } else {
+          passport.authenticate('local')(req, res, function () {
+            res.redirect('/secrets');
+          });
+        }
+      }
+    );
   });
 
 app.get('/logout', (req, res) => {
-  res.redirect('/');
+  req.logout((err) => {
+    err && console.log(err);
+
+    res.redirect('/');
+  });
 });
 
 app.listen(PORT, () => {
