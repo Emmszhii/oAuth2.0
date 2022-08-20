155569a12c5917587a212979846a1c703fc042ec
Emmszhii <emmanuelmanalo707@gmail.com>
2022-08-20 12:01:09 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,84 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const encrypt = require('mongoose-encryption');
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please insert a username!'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please insert a password!'],
+  },
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ['password'],
+});
+
+const User = new mongoose.model('User', userSchema);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app
+  .route('/login')
+  .get((req, res) => {
+    res.render('login');
+  })
+  .post((req, res) => {
+    User.findOne(
+      {
+        email: req.body.username,
+      },
+      (err, result) => {
+        err && console.log(err);
+
+        if (result) {
+          result.password === req.body.password
+            ? res.render('secrets')
+            : res.render('login');
+        }
+      }
+    );
+  });
+
+app
+  .route('/register')
+  .get((req, res) => {
+    res.render('register');
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+
+    newUser.save((err) => {
+      !err ? res.render('secrets') : console.log(err);
+    });
+  });
+
+app.get('/logout', (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and listening on PORT ${PORT}`);
+});
